7a763918cbe630aeeb2ea2c139464701b93002c0
Jonas B <JonasB1997@aol.de>
2024-10-28 10:16:15 +0100

    moved bwr implementation to its own class

--- esphome/components/waveshare_epaper/display.py
+++ esphome/components/waveshare_epaper/display.py
@@ -74,7 +74,10 @@ WaveshareEPaper7P5InBV2 = waveshare_epaper_ns.class_(
     "WaveshareEPaper7P5InBV2", WaveshareEPaper
 )
 WaveshareEPaper7P5InBV3 = waveshare_epaper_ns.class_(
-    "WaveshareEPaper7P5InBV3", WaveshareEPaperBWR
+    "WaveshareEPaper7P5InBV3", WaveshareEPaper
+)
+WaveshareEPaper7P5InBV3BWR = waveshare_epaper_ns.class_(
+    "WaveshareEPaper7P5InBV3BWR", WaveshareEPaperBWR
 )
 WaveshareEPaper7P5InV2 = waveshare_epaper_ns.class_(
     "WaveshareEPaper7P5InV2", WaveshareEPaper
@@ -129,6 +132,7 @@ MODELS = {
     "7.50in": ("b", WaveshareEPaper7P5In),
     "7.50in-bv2": ("b", WaveshareEPaper7P5InBV2),
     "7.50in-bv3": ("b", WaveshareEPaper7P5InBV3),
+    "7.50in-bv3-bwr": ("b", WaveshareEPaper7P5InBV3BWR),
     "7.50in-bc": ("b", WaveshareEPaper7P5InBC),
     "7.50inv2": ("b", WaveshareEPaper7P5InV2),
     "7.50inv2alt": ("b", WaveshareEPaper7P5InV2alt),
